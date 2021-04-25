401ecdce0917096927e0a57cba0565c150fdee81
Semibratov Ilya <i7brothers@gmail.com>
2021-04-25 11:07:29 +0400

    Initial commit

---
+++ src/ru/semibratov/Main.java
@@ -0,0 +1,8 @@
+package ru.semibratov;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/ru/semibratov/SocketRunner.java
@@ -0,0 +1,5 @@
+package ru.semibratov;
+
+public class SocketRunner {
+    
+}
